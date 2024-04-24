705a7c2e1b070f89d8017283e019346663a3fabc
FaisalAbubaker <faiabu95@gmail.com>
2024-04-24 16:34:30 +0300

    m

--- app/src/main/java/com/example/bmicalculator/MainActivity.kt
+++ app/src/main/java/com/example/bmicalculator/MainActivity.kt
@@ -18,6 +18,7 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.foundation.text.KeyboardOptions
@@ -80,151 +81,156 @@ fun CalcBMI() {
     var sliderPosition2 by remember { mutableFloatStateOf(1f) }
     var expanded by remember { mutableStateOf (false) }
     Box(contentAlignment = Alignment.Center) {
-        Column(
-            Modifier
-                .background(Color(0xFFe0ebba))
-                .padding(20.dp)
-                .fillMaxSize(), horizontalAlignment = Alignment.CenterHorizontally
-        ) {
-            Text(
-                text = "BMI Calculator", fontSize = 40.sp, fontWeight = FontWeight.Bold,
-                color = Color(0xFF38631d)
-            )
-            Spacer(Modifier.padding(15.dp))
-            Row(
+        LazyColumn(
                 Modifier
-                    .padding(10.dp)
-                    .fillMaxWidth(),
-                horizontalArrangement = Arrangement.Center,
-            ) {
-                var selected by remember { mutableStateOf(false) }
-                val pickColor = if (selected) 0xFFebe96b else 0xFF96d263
-                var selected2 by remember { mutableStateOf(false) }
-                val pickColor2 = if (selected2) 0xFFebe96b else 0xFF96d263
-                Button(
-                    onClick = {
-                        selected = !selected
-                        if (selected2 == true) {
-                            selected2 = !selected2
-                        }
-                        gender = 1
-                    }, Modifier.size(170.dp),
-                    colors = ButtonDefaults.buttonColors(containerColor = Color(pickColor))
+                    .background(Color(0xFFe0ebba))
+                    .padding(20.dp)
+                    .fillMaxSize(), horizontalAlignment = Alignment.CenterHorizontally
+        ) {
+            item {
+                Text(
+                    text = "BMI Calculator", fontSize = 40.sp, fontWeight = FontWeight.Bold,
+                    color = Color(0xFF38631d)
+                )
+                Spacer(Modifier.padding(15.dp))
+                Row(
+                    Modifier
+                        .padding(10.dp)
+                        .fillMaxWidth(),
+                    horizontalArrangement = Arrangement.Center,
                 ) {
-                    Icon(
-                        painter = painterResource(id = R.drawable.male),
-                        contentDescription = "male", tint = Color.Unspecified
-                    )
-                }
-                Spacer(Modifier.weight(1f))
-                Button(
-                    onClick = {
-                        selected2 = !selected2
-                        if (selected == true) {
+                    var selected by remember { mutableStateOf(false) }
+                    val pickColor = if (selected) 0xFFebe96b else 0xFF96d263
+                    var selected2 by remember { mutableStateOf(false) }
+                    val pickColor2 = if (selected2) 0xFFebe96b else 0xFF96d263
+                    Button(
+                        onClick = {
                             selected = !selected
-                        }
-                        gender = 2
-                    }, Modifier.size(170.dp),
-                    colors = ButtonDefaults.buttonColors(containerColor = Color(pickColor2))
-                ) {
-                    Icon(
-                        painter = painterResource(id = R.drawable.female),
-                        contentDescription = "female", tint = Color.Unspecified
-                    )
-                }
-            }
-
-            Text(
-                text = "Weight", fontSize = 35.sp, fontWeight = FontWeight.Bold,
-                color = Color(0xFF4d3316)
-            )
-            Slider(
-                valueRange = 1f..200f,
-                value = sliderPosition,
-                colors = SliderDefaults.colors(
-                    thumbColor = Color(0xFF96d263),
-                    activeTrackColor = Color(0xFF17501c),
-                    inactiveTrackColor = Color(0xFFb7e8b0),
-                ),
-                onValueChange = { sliderPosition = it }
-            )
-            Text(
-                text = "%.0f".format(sliderPosition) + "kg", fontSize = 25.sp,
-                fontWeight = FontWeight.Bold, color = Color(0xFF4d3316)
-            )
-            Spacer(Modifier.padding(20.dp))
-            Text(
-                text = "Height", fontSize = 35.sp, fontWeight = FontWeight.Bold,
-                color = Color(0xFF4d3316)
-            )
-            Slider(
-                valueRange = 1f..2f,
-                value = sliderPosition2,
-                colors = SliderDefaults.colors(
-                    thumbColor = Color(0xFF96d263),
-                    activeTrackColor = Color(0xFF17501c),
-                    inactiveTrackColor = Color(0xFFb7e8b0),
-                ),
-                onValueChange = { sliderPosition2 = it }
-            )
-            Text(
-                text = "%.2f".format(sliderPosition2) + "m", fontSize = 25.sp,
-                fontWeight = FontWeight.Bold, color = Color(0xFF4d3316)
-            )
-            Spacer(Modifier.padding(20.dp))
-            var age by remember { mutableStateOf("") }
-            Row(
-                horizontalArrangement = Arrangement.Center,
-                verticalAlignment = Alignment.CenterVertically
-            ) {
-                Column {
-                    Text(
-                        text = "Age", fontSize = 35.sp, fontWeight = FontWeight.Bold,
-                        color = Color(0xFF4d3316),
-                        modifier = Modifier.padding(start = 5.dp)
-                    )
-                    Spacer(Modifier.padding(10.dp))
-                    TextField(
-                        value = age, onValueChange = { age = it },
-                        keyboardOptions = KeyboardOptions.Default.copy(keyboardType = KeyboardType.Number),
-                        maxLines = 1,
-                        modifier = Modifier.size(80.dp)
-                            .border(width = 2.dp, color = Color(0xFF96d263)),
-                        textStyle = TextStyle.Default.copy(
-                            fontSize = 28.sp,
-                            fontWeight = FontWeight.Bold,
-                            color = Color(0xFF4d3316),
-                            textAlign = TextAlign.Center,
-                        ),
-                        colors = TextFieldDefaults.textFieldColors(
-                            cursorColor = Color.Transparent,
-                            containerColor = Color(0xFFb7e8b0),
+                            if (selected2 == true) {
+                                selected2 = !selected2
+                            }
+                            gender = 1
+                        }, Modifier.size(170.dp),
+                        colors = ButtonDefaults.buttonColors(containerColor = Color(pickColor))
+                    ) {
+                        Icon(
+                            painter = painterResource(id = R.drawable.male),
+                            contentDescription = "male", tint = Color.Unspecified
                         )
-                    )
+                    }
+                    Spacer(Modifier.weight(1f))
+                    Button(
+                        onClick = {
+                            selected2 = !selected2
+                            if (selected == true) {
+                                selected = !selected
+                            }
+                            gender = 2
+                        }, Modifier.size(170.dp),
+                        colors = ButtonDefaults.buttonColors(containerColor = Color(pickColor2))
+                    ) {
+                        Icon(
+                            painter = painterResource(id = R.drawable.female),
+                            contentDescription = "female", tint = Color.Unspecified
+                        )
+                    }
                 }
+
+                Text(
+                    text = "Weight", fontSize = 35.sp, fontWeight = FontWeight.Bold,
+                    color = Color(0xFF4d3316)
+                )
+                Slider(
+                    valueRange = 1f..200f,
+                    value = sliderPosition,
+                    colors = SliderDefaults.colors(
+                        thumbColor = Color(0xFF96d263),
+                        activeTrackColor = Color(0xFF17501c),
+                        inactiveTrackColor = Color(0xFFb7e8b0),
+                    ),
+                    onValueChange = { sliderPosition = it }
+                )
+                Text(
+                    text = "%.0f".format(sliderPosition) + "kg", fontSize = 25.sp,
+                    fontWeight = FontWeight.Bold, color = Color(0xFF4d3316)
+                )
                 Spacer(Modifier.padding(20.dp))
-                Button(
-                    onClick = { result = OnSubmit(sliderPosition, sliderPosition2, gender, age)
-                              if (expanded == false){
-                                  expanded = true
-                              }},
-                    colors = ButtonDefaults.buttonColors(containerColor = Color(0xFF96d263)),
-                    modifier = Modifier
-                        .size(height = 150.dp, width = 170.dp)
-                        .padding(start = 30.dp, top = 55.dp)
-                        .border(
-                            width = 3.dp,
-                            color = Color(0xFFebe96b)
-                        ),
-                    shape = RoundedCornerShape(0)
+                Text(
+                    text = "Height", fontSize = 35.sp, fontWeight = FontWeight.Bold,
+                    color = Color(0xFF4d3316)
+                )
+                Slider(
+                    valueRange = 1f..2f,
+                    value = sliderPosition2,
+                    colors = SliderDefaults.colors(
+                        thumbColor = Color(0xFF96d263),
+                        activeTrackColor = Color(0xFF17501c),
+                        inactiveTrackColor = Color(0xFFb7e8b0),
+                    ),
+                    onValueChange = { sliderPosition2 = it }
+                )
+                Text(
+                    text = "%.2f".format(sliderPosition2) + "m", fontSize = 25.sp,
+                    fontWeight = FontWeight.Bold, color = Color(0xFF4d3316)
+                )
+                Spacer(Modifier.padding(20.dp))
+                var age by remember { mutableStateOf("") }
+                Row(
+                    horizontalArrangement = Arrangement.Center,
+                    verticalAlignment = Alignment.CenterVertically
                 ) {
-                    Text(
-                        text = "Calculate BMI", fontWeight = FontWeight.Bold, fontSize = 20.sp,
-                        textAlign = TextAlign.Center
-                    )
+                    Column {
+                        Text(
+                            text = "Age", fontSize = 35.sp, fontWeight = FontWeight.Bold,
+                            color = Color(0xFF4d3316),
+                            modifier = Modifier.padding(start = 5.dp)
+                        )
+                        Spacer(Modifier.padding(10.dp))
+                        TextField(
+                            value = age, onValueChange = { age = it },
+                            keyboardOptions = KeyboardOptions.Default.copy(keyboardType = KeyboardType.Number),
+                            maxLines = 1,
+                            modifier = Modifier
+                                .size(80.dp)
+                                .border(width = 2.dp, color = Color(0xFF96d263)),
+                            textStyle = TextStyle.Default.copy(
+                                fontSize = 28.sp,
+                                fontWeight = FontWeight.Bold,
+                                color = Color(0xFF4d3316),
+                                textAlign = TextAlign.Center,
+                            ),
+                            colors = TextFieldDefaults.colors(
+                                cursorColor = Color.Transparent,
+//                                containerColor = Color(0xFFb7e8b0),
+                            )
+                        )
+                    }
+                    Spacer(Modifier.padding(20.dp))
+                    Button(
+                        onClick = {
+                            result = OnSubmit(sliderPosition, sliderPosition2, gender, age)
+                            if (expanded == false) {
+                                expanded = true
+                            }
+                        },
+                        colors = ButtonDefaults.buttonColors(containerColor = Color(0xFF96d263)),
+                        modifier = Modifier
+                            .size(height = 150.dp, width = 170.dp)
+                            .padding(start = 30.dp, top = 55.dp)
+                            .border(
+                                width = 3.dp,
+                                color = Color(0xFFebe96b)
+                            ),
+                        shape = RoundedCornerShape(0)
+                    ) {
+                        Text(
+                            text = "Calculate BMI", fontWeight = FontWeight.Bold, fontSize = 20.sp,
+                            textAlign = TextAlign.Center
+                        )
+                    }
                 }
-            }
 //            Text(text = result, fontSize = 25.sp)
+            }
         }
         var bmi = sliderPosition/(sliderPosition2*sliderPosition2)
         var col = 0xFFFFFFFF
@@ -254,13 +260,15 @@ fun CalcBMI() {
             colors = CardDefaults.cardColors(
                 containerColor = Color(col))
         ) {
-        // Card content goes here, at this state a column to hold items
-            if(expanded) {
-                Image(painter = painterResource(id = img), contentDescription = "",
+            // Card content goes here, at this state a column to hold items
+            if (expanded) {
+                Image(
+                    painter = painterResource(id = img), contentDescription = "",
                     Modifier
                         .size(70.dp)
                         .padding(5.dp)
-                        .align(Alignment.CenterHorizontally))
+                        .align(Alignment.CenterHorizontally)
+                )
                 Text(
                     text = "Report", fontSize = 30.sp, fontWeight = FontWeight.Bold,
                     modifier = Modifier
@@ -273,9 +281,7 @@ fun CalcBMI() {
                 )
             }
 
-
-    }
-
+        }
     }
 }
 
